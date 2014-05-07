26c136e1c3e6dcb533db6a3efe2dbc70b0b3f2cb
Markus Teich <markus.teich@stusta.mhn.de>
2014-05-07 21:56:53 +0200

    first prototypes

--- client/client.c
+++ client/client.c
@@ -1,4 +1,22 @@
 
+int send_all(const char* message, const char* room);
+int send_user(const char* message, const char* user);
+int main(int argc, char* argv[]);
+
+// sends the message to all clients in room
+int
+send_all(const char* message, const char* room)
+{
+	return 0;
+}
+
+// sends the message to the user
+int
+send_user(const char* message, const char* user)
+{
+	return 0;
+}
+
 int
 main(int argc, char* argv[])
 {
