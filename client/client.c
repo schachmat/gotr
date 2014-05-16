15bc1b565273b96536e6c8ed152f6a8e5f4773be
Markus Teich <markus.teich@stusta.mhn.de>
2014-05-16 15:40:34 +0200

    fix function prototypes

--- client/client.c
+++ client/client.c
@@ -3,14 +3,14 @@
 // variables
 static char* nick;
 
-// predeclarations
-int send_all(const char* message, const char* room);
+// prototypes
+int send_all(const char* message);
 int send_user(const char* message, const char* user);
 int main(int argc, char* argv[]);
 
-// sends the message to all clients in room
+// sends the message to all clients in the room
 int
-send_all(const char* message, const char* room)
+send_all(const char* message)
 {
 	return 0;
 }
