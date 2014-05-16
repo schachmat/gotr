4c091d42b86b60eac926663adc6ae7dce230c8c2
Markus Teich <markus.teich@stusta.mhn.de>
2014-05-16 15:37:10 +0200

    argparsing done

--- client/client.c
+++ client/client.c
@@ -1,4 +1,9 @@
+#include <stdio.h>
 
+// variables
+static char* nick;
+
+// predeclarations
 int send_all(const char* message, const char* room);
 int send_user(const char* message, const char* user);
 int main(int argc, char* argv[]);
@@ -20,5 +25,13 @@ send_user(const char* message, const char* user)
 int
 main(int argc, char* argv[])
 {
+	if(argc < 2) {
+		fprintf(stderr, "client nickname required");
+		return 1;
+	}
+
+	nick = argv[1];
+	printf("nick: %s", nick);
+
 	return 0;
 }
