28aeed7b15cbfb691eed87c3ebbf8aa0478a98da
Jannik Theiß <dev@coldsun.org>
2014-05-30 13:41:33 +0200

    auto add user

--- libgotr/libgotr.c
+++ libgotr/libgotr.c
@@ -6,10 +6,11 @@
 
 void gotr_setup(struct gotr_chatroom *room)
 {
-	char setup_message[crypto_box_PUBLICKEYBYTES + 1];
+	char setup_message[crypto_box_PUBLICKEYBYTES + 2];
 	
 	crypto_box_keypair(room->pub_key, room->sec_key);
 	setup_message[0] = '\\';
+	setup_message[crypto_box_PUBLICKEYBYTES + 1] = '\0';
 	memcpy(room->pub_key, setup_message + 1, crypto_box_PUBLICKEYBYTES);
 	
 	room->send_all(setup_message);
@@ -59,6 +60,8 @@ void gotr_add_user(struct gotr_chatroom *room, char *pub_key)
 	room->users = new_user;
 	
 	memcpy(new_user->pub_key, pub_key, crypto_box_PUBLICKEYBYTES);
+	
+	room->receive_usr(room, "-- user added", pub_key); //debug
 }
 
 void gotr_leave(struct gotr_chatroom *room)
