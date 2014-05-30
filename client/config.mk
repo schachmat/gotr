0312ab3d701355b1704c15b73672a36694d0abd7
Markus Teich <markus.teich@stusta.mhn.de>
2014-05-30 13:53:43 +0200

    use POSIX 2001

--- client/config.mk
+++ client/config.mk
@@ -1,12 +1,13 @@
 # Customize below to fit your system
 
 # includes and libs
-INCS = -I. -I/usr/include -I../libgotr
-LIBS = -L/usr/lib -lc -L../libgotr -lgotr -lnacl
+INCS = -I. -I/usr/include -I../libgotr -I../libgotr/include
+LIBS = -L/usr/lib -lc -L../libgotr -lgotr
 
 # flags
-CFLAGS = -g -pedantic -Wall -O0 ${INCS}
-#CFLAGS = -pedantic -Wall -O3 ${INCS}
+CPPFLAGS = -D_XOPEN_SOURCE=600
+CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+#CFLAGS = -std=c99 -pedantic -Wall -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS = -g ${LIBS}
 #LDFLAGS = -s ${LIBS}
 
