0655ea1873e10fe347a027f0682cfb3be95940ef
Markus Teich <markus.teich@stusta.mhn.de>
2014-06-04 16:46:10 +0200

    move client into libgotr dir

--- libgotr/config.mk
+++ libgotr/config.mk
@@ -5,11 +5,16 @@ INCS = -I. -I./include -I/usr/include
 LIBS = -L./lib -L/usr/lib -lc -lnacl
 NACL_LOCATION = "lib/libnacl.a"
 
+CLIENT_LIBS = -L. -L/usr/lib -lc -lgotr
+
 # flags
 CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS}
 #CFLAGS = -std=c99 -pedantic -Wall -O3 ${INCS}
 LDFLAGS = -g ${LIBS}
 #LDFLAGS = -s ${LIBS}
+
+CLIENT_LDFLAGS = -g ${CLIENT_LIBS}
+#CLIENT_LDFLAGS = -s ${CLIENT_LIBS}
 
 # compiler and linker
 CC = cc
