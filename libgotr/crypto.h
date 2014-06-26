a02770f7da3c26d77652cc9e59f0ea50ea099d15
Markus Teich <markus.teich@stusta.mhn.de>
2014-06-26 23:39:23 +0200

    apply license header to crypto.[ch]

--- libgotr/crypto.h
+++ libgotr/crypto.h
@@ -1,3 +1,24 @@
+/**
+ * This file is part of libgotr.
+ * (C) 2001-2014 Christian Grothoff, Nils Durner, Markus Teich, Jannik Theiß
+ * (and other contributing authors)
+ *
+ * libgotr is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your
+ * option) any later version.
+ *
+ * libgotr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libgotr; see the file LICENSE.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 
 #ifndef _GOTR_CRYPTO_H
 #define _GOTR_CRYPTO_H
