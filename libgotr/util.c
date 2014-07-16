b13e008208dfc0443e1d994764b0b814fc6b74e8
Markus Teich <markus.teich@stusta.mhn.de>
2014-07-16 22:25:14 +0200

    add assertion functions.
    They should be used in every unexported libgotr function to avoid having to deal
    with int status return codes everywhere.

--- libgotr/util.c
+++ libgotr/util.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <gcrypt.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -23,4 +24,22 @@ void gotr_eprintf(const char *format, ...)
 	} else {
 		fputc('\n', stderr);
 	}
+}
+
+void gotr_assert_fail(const char *assertion, const char *file, unsigned int line, const char *function)
+{
+	gotr_eprintf("Assertion failed in file %s line %d function %s: %s", file, line, function, assertion);
+	abort();
+}
+
+void gotr_assert_perror_fail(int errnum, const char *file, unsigned int line, const char *function)
+{
+	gotr_eprintf("Assertion failed in file %s line %d function %s:", file, line, function);
+	abort();
+}
+
+void gotr_assert_gpgerr_fail(gcry_error_t errnum, const char *file, unsigned int line, const char *function)
+{
+	gotr_eprintf("Assertion failed in file %s line %d function %s: %s", file, line, function, gcry_strerror(errnum));
+	abort();
 }
--- libgotr/util.h
+++ libgotr/util.h
@@ -1,8 +1,32 @@
 #ifndef _GOTR_UTIL_H
 #define _GOTR_UTIL_H
 
+#include <gcrypt.h>
+
 #define GOTR_OK 1
 
 void gotr_eprintf(const char *format, ...);
+
+# undef gotr_assert
+# undef gotr_assert_perror
+# undef gotr_assert_gpgerr
+
+#ifdef NDEBUG
+
+# define gotr_assert(expr)          ((void)(0))
+# define gotr_assert_perror(errnum) ((void)(0))
+# define gotr_assert_gpgerr(errnum) ((void)(0))
+
+#else /* NDEBUG */
+
+void gotr_assert_fail(const char *assertion, const char *file, unsigned int line, const char *function);
+void gotr_assert_perror_fail(int errnum, const char *file, unsigned int line, const char *function);
+void gotr_assert_gpgerr_fail(gcry_error_t errnum, const char *file, unsigned int line, const char *function);
+
+# define gotr_assert(expr)          ((expr)    ? (void)(0) : gotr_assert_fail ((#expr), __FILE__, __LINE__, __PRETTY_FUNCTION__))
+# define gotr_assert_perror(errnum) (!(errnum) ? (void)(0) : gotr_assert_perror_fail ((errnum), __FILE__, __LINE__, __PRETTY_FUNCTION__))
+# define gotr_assert_gpgerr(errnum) (!(errnum) ? (void)(0) : gotr_assert_gpgerr_fail ((errnum), __FILE__, __LINE__, __PRETTY_FUNCTION__))
+
+#endif /* NDEBUG */
 
 #endif
