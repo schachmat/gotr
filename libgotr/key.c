e70da58d7497f21b159fc830ced093b893ed063a
Markus Teich <markus.teich@stusta.mhn.de>
2014-08-04 13:55:14 +0200

    rework private key storage

--- libgotr/key.c
+++ libgotr/key.c
@@ -12,26 +12,36 @@ void load_privkey(const char* abs_filename, struct gotr_dsa_skey *key)
 	size_t size = sizeof(struct gotr_dsa_skey);
 	mode_t oldmask;
 
-	if ((fp = fopen(abs_filename, "rb"))) {
-		if (fread(key, 1, size, fp) == size) {
-			fclose(fp);
-			return;
-		}
-		fclose(fp);
+	if (!abs_filename)
+		goto create;
+
+	if (!(fp = fopen(abs_filename, "rb"))) {
+		gotr_eprintf("could not open file %s for reading:", abs_filename);
+		goto create;
 	}
 
-	gotr_eprintf("could not load private key from file:");
-	gotr_eprintf("generating new private key, please wait...");
+	if (size == fread(key, 1, size, fp)) {
+		fclose(fp);
+		return;
+	}
+	fclose(fp);
+	gotr_eprintf("could not read private key from file %s", abs_filename);
+
+create:
+//	gotr_eprintf("generating new private key, please wait...");
 	gotr_eddsa_key_create(key);
-	gotr_eprintf("done generating private key.");
+//	gotr_eprintf("done generating private key.");
+
+	if (!abs_filename)
+		return;
 
 	oldmask = umask(077);
 	if ((fp = fopen(abs_filename, "wb"))) {
 		if (fwrite(key, 1, size, fp) != size)
-			gotr_eprintf("could not save private key to file");
+			gotr_eprintf("could not write private key to file %s", abs_filename);
 		fclose(fp);
 	} else {
-		gotr_eprintf("could not open file for writing:");
+		gotr_eprintf("could not open file %s for writing:", abs_filename);
 	}
 	umask(oldmask);
 }
