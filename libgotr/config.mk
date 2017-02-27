c8427c89fac7f59057e32827a7bb946298ac6b84
Markus Teich <markus.teich@stusta.mhn.de>
2017-02-27 18:51:19 +0100

    set DEFAULT_SOURCE

--- libgotr/config.mk
+++ libgotr/config.mk
@@ -10,7 +10,7 @@ LIBS = -L/usr/lib -lc -lgcrypt
 CLIENT_LIBS = -L. -L/usr/lib -lc -lgotr -lgcrypt
 
 # flags
-CPPFLAGS = -DLGOTR_VERSION_MAJOR=\"${MAJOR}\" -DLGOTR_VERSION_MINOR=\"${MINOR}\" -DGOTR_GCRYPT_VERSION=\"1.6.1\"
+CPPFLAGS = -DLGOTR_VERSION_MAJOR=\"${MAJOR}\" -DLGOTR_VERSION_MINOR=\"${MINOR}\" -DGOTR_GCRYPT_VERSION=\"1.6.1\" -D_DEFAULT_SOURCE
 CFLAGS = -g -std=c99 -fPIC -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 #CFLAGS = -std=c99 -fPIC -pedantic -Wall -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS = -g -shared -Wl,-soname,libgotr.so.${MAJOR} ${LIBS}
